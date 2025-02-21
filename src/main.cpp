dd682010931c030d8003f365c3aa880b32f2a9e8
Ekaterina Bykova <kyb0212@mail.ru>
2025-02-21 11:29:59 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+extern double add(double lhs double rhs)
+extern double subtract(double lhs double rhs)
+
+int main() {
+    double a, b
+    std::cout << "Введите первое число: ";
+    std::cin >> a;
+    std::cout << "Введите второе число: ";
+    std::cin >> b;
+    double sum = add(a, b);
+    double difference = subtract(a, b);
+
+    std::cout << "Сумма: " << sum << std::endl;
+    std::cout << "Разность: " << difference << std::endl;
+
+    return 0;
+}
---
+++ src/utils.cpp
@@ -0,0 +1,7 @@
+double add(double lhs, double rhs) {
+    return lhs + rhs;
+}
+
+double subtract(double lhs, double rhs) {
+    return lhs - rhs;
+}
